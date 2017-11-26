7b52218146e6ac7637329d6893c9d6a42dfa0629
JiaLiang117 <31989922+JiaLiang117@users.noreply.github.com>
2017-11-26 19:49:57 +0800

    edits

--- trading/Main.py
+++ trading/Main.py
@@ -1,8 +1,40 @@
+import pandas as pd
+
 class Trading:
 
 
     def __init__(self):
-        self.data = [10]
+        self.CI=pd.DataFrame(columns=["ticker","position","quantity","Open","Current_Close","OpenDate","CloseDate",
+                                       ])
+
+
+        self.PnL=pd.DataFrame(columns=[])
+
+        pass
+
+    def stock_trade_cityindex(self, ticker, price, quantity, direction,commission, currency):
+        """
+        ticker (str) for standard chartered currency should contain only the target currency
+
+        quantity (int) number of stocks or currency traded
+        direction (int) long=1, short=-1
+        margin  (float) percentage of margin required
+
+        :param ticker: (str) for standard chartered currency should contain only the target currency
+        :param price:  (float) trade price
+        :param quantity: (int) number of stocks or currency traded
+        :param direction: (int) long=1, short=-1
+        :param commission: (float)
+        :param currency: (str)  currency of stock
+        :param rate: (float)    currency rate
+
+        :return: None
+        """
+
+
+        dasfdsa
+
+
 
 
 
